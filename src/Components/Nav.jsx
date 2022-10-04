2fbec8e2de86549125f1cbf1f0c37529198dfe3d
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-10-04 18:19:31 +0100

    create login page for kaban store and styled it

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaUserAlt } from "react-icons/fa";
 import  {GiShoppingCart } from "react-icons/gi"
+import { useNavigate } from 'react-router-dom';
 import logo from "../assets/logo.png"
 
 function Nav() {
@@ -12,28 +13,34 @@ function Nav() {
   const handleSubmit = (e)=>(
      e.preventDefault()
      
-  )
+  );
+  
+  const navigate = useNavigate()
   
   return (
-   <div className='w-full text-[#424b52] h-4xl bg-[#fff] shadow-lg '>
+   <div className='w-full text-[#424b52] h-2xl bg-[#fff] shadow-lg '>
      <nav className='max-w-6xl flex justify-between items-center mx-auto p-4'>
        
-        <img src={logo} alt='Kaban Logo' className='w-[70px] cursor-pointer '/>
+        <img src={logo} alt='Kaban Logo' className='h-[90px] cursor-pointer mr-[7px]'/>
         
         <form onSubmit={handleSubmit} className='relative flex items-center '>
            <AiOutlineSearch className='absolute left-0 text-[1.3rem] fw-bold ml-[5px] '/>
            <input onChange={handleChange} type="search" value={search} id="search" placeholder='Search products' className=' rounded text-sm border-sm border-[#5b7e99] md:w-[400px]  lg:w-[600px] pl-[2rem] focus:border-[#1e6091]' />
-           <button className=' shadow ml-[10px] uppercase mx-auto  md:w-[6rem] py-[10px] px-[15px] bg-[#1e6091] text-white rounded h-[40px] flex items-center justify-center cursor-pointer transition duration-150 ease-in-out hover:bg-[#102f47]'> search </button>
+           <button className=' shadow ml-[10px] uppercase mx-auto  md:w-[9rem] py-[10px] px-[15px] bg-[#1e6091] text-white rounded h-[40px] flex items-center justify-center cursor-pointer transition duration-150 ease-in-out hover:bg-[#102f47]'> search </button>
         </form>
        
        
-        <div className='flex justify-between items-center cursor-pointer m-[5px] hover:text-[#1e6091]'>
+        <div onClick={()=>{
+          navigate("/login")
+        }} className='flex justify-between items-center cursor-pointer m-[5px] hover:text-[#1e6091]'>
             <FaUserAlt className=' pr-[5px] text-3xl'/>
             <p className='hidden md:block'>Login</p>
             
           
         </div>
-        <div className='flex cursor-pointer items-center hover:text-[#1e6091]'>
+        <div onClick={()=>{
+          navigate("/cart")
+        }} className='flex cursor-pointer items-center hover:text-[#1e6091]'>
             <GiShoppingCart className=' pr-[5px] text-4xl'/>
             <p className='hidden md:block'>Cart</p>
         </div>
