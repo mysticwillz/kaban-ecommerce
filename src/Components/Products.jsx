41448482ce7edb44f0c40a9564694d6b6a8d7bcf
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-10-16 18:26:40 +0100

    create shopData folder to store all the data and updated the stor info

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import one from "../kabanhero/one.png"
 import two from "../kabanhero/two.png"
@@ -18,36 +18,29 @@ import "swiper/css/bundle";
 
 // import required modules
 import { Pagination, Navigation } from "swiper";
-import shopData from "./Data.jsx"
+import allData from "./shopData/Data";
+import topSellingItemData from "./shopData/TopSellingItems";
+import kabanUniversalWeekData from "./shopData/KabanUniversalWeek";
+
 
 
 
 export default function App() {
-  console.log(shopData)
+  const [myShopData, setMyShopData ] = useState(allData)
+  const [ topSelling, setTopSelling] = useState(topSellingItemData)
+  const [ kabanWeek, setKabanWeek] = useState(kabanUniversalWeekData)
 
-  const pictures = [{img:one,alt:"first hero",
-id:1},
-  {img:two,alt:"second hero",id:2},
-  {img:three, alt:"third hero",id:3},
-  {img:four,alt:"fourth hero",id:4},
-  {img:five, alt:"fifth hero",id:5}
-  ];
-
-  const picture = [
-    {img:one,alt:"first hero",id:1},
-  {img:two,alt:"second hero",id:2}, 
-  {img:six,alt:"sixth hero",id:6}];
 
   return (
     <section className="max-w-7xl mx-auto">
      
-     <div className="flex flex-col justify-between mx-auto max-w-7xl bg-white  mt-[10px] ">
+     <div className="flex flex-col justify-between mx-auto h-[250px] md:h-[300px] bg-white  mt-[10px] ">
 
-     <div className="w-full bg-black rounded-t-sm">
+     <div className="w-full bg-[#1e6091] rounded-t-sm">
        <h1 className="text-center text-white text-[20px] md:text-[30px]  capitalize"> top selling items </h1>
        </div>
 
-       <div >
+       <div className=" h-[80%]" >
         
       <Swiper
         slidesPerView={3}
@@ -63,16 +56,17 @@ id:1},
         className="mySwiper"
       >
        
-        {pictures.map((image)=>{
-          const {img, alt, id} = image
+        {topSelling.map((data)=>{
+          const {img, price, name,  id} = data
 
-          return <SwiperSlide key={id}className="flex flex-col items-center  product-div shadow-light-50 "  >
-            <div>
-            <img src={img} alt={alt}/>
-            <h1 className="text-bold ">title</h1>
-            <p className="my-[5px]">$10.5</p>
-        
-            <strike className=" text-[12px] text-warm-gray-400">$21</strike>
+          return <SwiperSlide key={id} className="flex flex-col items-center  product-div shadow-light-50 "  >
+            <div className="h-full">
+            <img src={img} alt="top selling " className=" h-[65%]"/>
+            <div className=" h-auto">
+           <p className="text-bold] px-1">{name}</p>
+            <p className=" text-[12px] md:text-[14px px-1">${price}</p>
+            <strike className=" px-1 pb-1 text-[10px] md:text-[12px] text-warm-gray-400">${price * 2}</strike>
+           </div>
             </div>
             </SwiperSlide>
         })}
@@ -82,24 +76,27 @@ id:1},
       </div>
       </div>
 
-     <div className="w-full flex flex-col  bg-white mt-[10px]">
+     <div className="w-full flex flex-col h-[250px] md:h-[300px] bg-white mt-[10px]">
       
-     <div className="w-full bg-[#14b390a9] rounded-t-sm">
-       <h1 className="text-center text-[20px] md:text-[30px] text-white capitalize">Kaban universal week</h1>
+     <div className="w-full bg-[#1e6091] h-[20%] flex items-center justify-center rounded-t-sm">
+       <h1 className="text-center text-[20px] md:text-[30px] text-white   capitalize">Kaban universal week</h1>
        </div>
 
-      <div className=" flex justify-between">
+      <div className=" flex h-[80%] justify-between">
         
-      {picture.map((image)=>{
-          const {img, alt, id} = image
+      {kabanWeek.map((data)=>{
+        const {img, price, name,  id} = data
 
-          return <div key={id} className=" flex justify-center  items-center " >
-          <div className=" product-div shadow-sm">
-           <img src={img} alt={alt} className="w-[400px]"/>
+          return <div key={id} className="  flex justify-center  items-center " >
+          <div className=" product-div h-full shadow-sm">
+           <img src={img} alt="kabanweek" className="w-[400px] h-[65%]"/>
             
-            <h1 className="text-bold px-1">title</h1>
-            <p className="my-[5px] px-1">$10.5</p>
-            <strike className=" px-1 pb-1 text-[12px] text-warm-gray-400">$21</strike>
+           <div className=" h-auto mt-1
+           ">
+           <p className="text-bold] px-1">{name}</p>
+            <p className=" text-[12px] md:text-[14px px-1">${price}</p>
+            <strike className=" px-1 pb-1 text-[10px] md:text-[12px] text-warm-gray-400">${price * 2}</strike>
+           </div>
             
            </div>
             </div>
@@ -110,43 +107,47 @@ id:1},
      
       <div className="w-full flex flex-col  bg-white mt-[10px]">
       
-      <div className="w-full bg-[blue]  rounded-t-sm">
+      <div className="w-full bg-[#1e6691]  rounded-t-sm">
        <h1 className="text-center text-white text-[20px] md:text-[30px]   capitalize">top deals </h1>
        </div>
      
 
-      <div className=" flex justify-between border-[1px] border-light-800 border-solid">
+      <div className=" flex justify-between border-[1px]  h-[80%] border-light-800 border-solid">
         
-      {pictures.map((image)=>{
-          const {img, alt, id} = image
+      {topSelling.map((data)=>{
+        const {img, price, name,  id} = data
 
-          return <div key={id} className=" flex justify-center  items-center " >
-          <div className=" product-div shadow-light-50">
-           <img src={img} alt={alt} className="w-[200px]"/>
-            <h1 className="text-bold ">title</h1>
-            <p className="my-[5px]">$10.5</p>
-            <strike className=" text-[12px] text-warm-gray-400">$21</strike>
-           </div>
-            </div>
+        return <div key={id} className="  flex justify-center  items-center " >
+        <div className=" product-div h-full shadow-sm">
+         <img src={img} alt="top selling" className="w-[400px] h-[65%]"/>
+          
+         <div className=" mt-1 h-auto">
+         <p className="text-bold] px-1">{name}</p>
+          <p className=" text-[12px] md:text-[14px px-1">${price}</p>
+          <strike className=" px-1 pb-1 text-[10px] md:text-[12px] text-warm-gray-400">${price * 2}</strike>
+         </div>
+          
+         </div>
+          </div>
         })}
       </div>
       </div>
     
       <div className="w-full flex flex-col bg-white  mt-[10px]">
       
-       <div className="w-full bg-[#e71c1ca9] rounded-t-sm">
-       <h1 className="text-center text-[20px] md:text-[30px]  capitalize">your favorite stores </h1>
+       <div className="w-full bg-[#1e6091] rounded-t-sm">
+       <h1 className="text-center text-white text-[20px] md:text-[30px]  capitalize">your favorite stores </h1>
        </div>
      
 
       <div className=" flex justify-between  ">
         
-      {picture.map((image)=>{
-          const {img, alt, id} = image
+      {kabanWeek.map((data)=>{
+           const {img,  id} = data
 
-          return <div key={id} className=" flex justify-between mb-[15px] items-center flex-wrap" >
+          return <div key={id} className=" flex justify-between text-white mb-[15px] items-center flex-wrap" >
            <div className=" product-div  shadow-light-50">
-           <img src={img} alt={alt} className="w-[400px] "/>
+           <img src={img} alt="favorite stores" className="w-[400px] "/>
             
             
            </div>
