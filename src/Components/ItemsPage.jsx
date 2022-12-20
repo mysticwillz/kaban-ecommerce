f0c1e42bba7a4cdfb713db18ed965130b414f036
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-12-20 22:06:11 +0100

    styled:styled the image component of the items page

--- src/Components/ItemsPage.jsx
+++ src/Components/ItemsPage.jsx
@@ -1,31 +1,32 @@
 import React from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
 function ItemsPage() {
   return (
     <main className="max-w-7xl mx-auto ">
-      <section className="flex items-center justify-between w-full border h-screen ">
-        <div className="flex flex-col items-center justify-center w-[50%] border   ">
+      <section className="flex items-center justify-between w-full  h-screen ">
+        <div className="flex flex-col items-center justify-center w-[50%]   ">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmR4jkEpjxw8UNqthaL6bcKZTivc8444J5vA&usqp=CAU"
             alt="item description"
             className="w-[500px] h-[400px]"
           />
           <div className="flex  items-center justify-center ">
-            <div className="flex  items-center justify-center w-[70px] h-[70px] border border-blue-400">
+            <div className="flex  items-center justify-center w-[50px] h-[50px] border border-blue-400">
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmR4jkEpjxw8UNqthaL6bcKZTivc8444J5vA&usqp=CAU"
                 alt="item description"
                 className="flex  w-[50px] h-[50px] "
               />
             </div>
-            <div className="flex  items-center justify-center w-[70px] h-[70px] m-4 border border-blue-400">
+            <div className="flex  items-center justify-center w-[50px] h-[50px] m-4 border border-blue-400">
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmR4jkEpjxw8UNqthaL6bcKZTivc8444J5vA&usqp=CAU"
                 alt="item description"
                 className="flex  w-[50px] h-[50px] "
               />
             </div>
-            <div className="flex  items-center justify-center w-[70px] h-[70px] border border-blue-400">
+            <div className="flex  items-center justify-center w-[50px] h-[50px] border border-blue-400">
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmR4jkEpjxw8UNqthaL6bcKZTivc8444J5vA&usqp=CAU"
                 alt="item description"
@@ -34,8 +35,30 @@ function ItemsPage() {
             </div>
           </div>
         </div>
-        <article className="flex items-center justify-between w-[50%] border ">
-          fffffffff
+        <article className=" w-[50%] h-full ">
+          <t2 className="text-[38px] font-bold py-6 ">
+            Oraimo Wireless Earbuds
+          </t2>
+
+          <div className="flex items-center  text-[18px] border-y w-full py-6 ">
+            <t5 className=" text-[32px] ">$800</t5>
+            <strike className="text-[17px] text-[#767879db] pl-6">$50</strike>
+          </div>
+          <div className="flex items-center py-6  ">
+            <h4>Quantity:</h4>
+            <div className="flex items-center border ml-6  ">
+              <AiOutlineMinus className=" text-[18px] font-bold cursor-pointer " />
+              <p className=" text-[18px] font-bold px-4  ">23</p>
+              <AiOutlinePlus className=" text-[18px] font-bold cursor-pointer  " />
+            </div>
+          </div>
+          <p className="text-[#767879db] ">Call us for bulk purchases:</p>
+          <p className=" text-[18px] font-bold cursor-pointer border-b pb-6 ">
+            Click here to show phone number
+          </p>
+          <button className="text-[18px] rounded my-6 border border-[#1e6091] bg-[#1e6091] text-white hover:bg-[#fff] hover:text-[#1e6091] transition-all duration-300 ease-in flex justify-center items-center h-[40px] w-full">
+            Add to cart
+          </button>
         </article>
       </section>
     </main>
