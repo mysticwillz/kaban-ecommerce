142020afa27af7a7edd877ba9c77c81cb435929a
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2023-01-06 22:35:04 +0100

    feat: this commit structures the order component of the checkout page

---
+++ src/Components/checkout/Order.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Order() {
+  return <aside className=" w-[300px] h-screen bg-white">Order</aside>;
+}
+
+export default Order;
--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 
 import { useNavigate } from "react-router-dom";
 import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import Order from "../Components/checkout/Order";
 
 function Checkout() {
   const navigate = useNavigate();
@@ -48,113 +49,107 @@ function Checkout() {
     }
   };
   return (
-    <>
-      <div
-        onClick={() => {
-          navigate("/cart");
-        }}
-        className=" ml-4 mb-2 text-[18px] rounded border border-[#1e6091] text-[#1e6091] cursor-pointer mt-10 hover:text-white hover:bg-[#1e6091] transition-all duration-300 ease-in flex justify-between  px-2 items-center h-[40px] w-[180px] "
-      >
-        <BsFillArrowLeftCircleFill className="  w-[30px]" />
-        <p> Back To Cart </p>
-      </div>
-      <main className="max-w-[30rem] mx-auto mt-[10px]">
-        <header className="flex items-center justify-center my-[10px] ">
-          <h1 className="text-center text-[2.3rem] font-bold ">Payment</h1>
-        </header>
-        <form onSubmit={handleSubmit} className="flex flex-col mt-3  w-full">
-          <div className="w-full   flex  flex-wrap md:flex-nowrap justify-between items-center  px-4 ">
-            <div className="flex flex-col  w-full  items-start  r">
-              <label htmlFor="name" className="font-bold pr-2 ">
-                Delivery Address:
-              </label>
-              <input
-                type="text"
-                required
-                id="name"
-                onChange={handleChange}
-                value={name}
-                placeholder="Enter delivery address"
-                className="w-full rounded border-0 focus:border-[#1e6091] "
-              />
+    <main className=" max-w-7xl ">
+      <section className="flex items-center justify-center max-w-4xl my-9 mx-auto bg-[#6c757d] rounded ">
+        <main className="max-w-[30rem] mx-auto mt-[10px]">
+          <header className="flex items-center justify-center my-[10px] text-white">
+            <h1 className="text-center text-[2.3rem] font-bold ">Payment</h1>
+          </header>
+          <form onSubmit={handleSubmit} className="flex flex-col mt-3  w-full">
+            <div className="w-full   flex  flex-wrap md:flex-nowrap justify-between items-center  px-4 ">
+              <div className="flex flex-col  w-full  items-start  r">
+                <label htmlFor="name" className="font-bold pr-2 ">
+                  Delivery Address:
+                </label>
+                <input
+                  type="text"
+                  required
+                  id="name"
+                  onChange={handleChange}
+                  value={name}
+                  placeholder="Enter delivery address"
+                  className="w-full rounded border-0 focus:border-[#1e6091] "
+                />
+              </div>
             </div>
-          </div>
-          <div className="w-full mt-[10px]   flex  flex-wrap md:flex-nowrap justify-between items-center  px-4 ">
-            <div className="flex flex-col  w-full  items-start  r">
-              <label htmlFor="name" className="font-bold pr-2 ">
-                CardHolder's Name:
-              </label>
-              <input
-                type="text"
-                required
-                id="name"
-                onChange={handleChange}
-                value={name}
-                placeholder="Name on card"
-                className="w-full rounded border-0 focus:border-[#1e6091] "
-              />
+            <div className="w-full mt-[10px]   flex  flex-wrap md:flex-nowrap justify-between items-center  px-4 ">
+              <div className="flex flex-col  w-full  items-start  r">
+                <label htmlFor="name" className="font-bold pr-2 ">
+                  CardHolder's Name:
+                </label>
+                <input
+                  type="text"
+                  required
+                  id="name"
+                  onChange={handleChange}
+                  value={name}
+                  placeholder="Name on card"
+                  className="w-full rounded border-0 focus:border-[#1e6091] "
+                />
+              </div>
             </div>
-          </div>
-          <div className="w-full mt-[10px]   flex  flex-wrap md:flex-nowrap justify-between items-center  px-4 ">
-            <div className="flex flex-col  w-full  items-start  r">
-              <label htmlFor="name" className="font-bold pr-2 ">
-                Card Number:
-              </label>
-              <input
-                type="text"
-                required
-                id="name"
-                onChange={handleChange}
-                value={name}
-                placeholder="Enter card number"
-                className="w-full rounded border-0 focus:border-[#1e6091] text-[20px] font-bold  letter-spacing "
-              />
+            <div className="w-full mt-[10px]   flex  flex-wrap md:flex-nowrap justify-between items-center  px-4 ">
+              <div className="flex flex-col  w-full  items-start  r">
+                <label htmlFor="name" className="font-bold pr-2 ">
+                  Card Number:
+                </label>
+                <input
+                  type="text"
+                  required
+                  id="name"
+                  onChange={handleChange}
+                  value={name}
+                  placeholder="Enter card number"
+                  className="w-full rounded border-0 focus:border-[#1e6091] text-[20px] font-bold  letter-spacing "
+                />
+              </div>
             </div>
-          </div>
 
-          <div className="w-full   flex  md:flex-nowrap justify-between items-center mt-[10px]  px-4 ">
-            <div className="flex flex-col  items-start w-[49%] ">
-              <label htmlFor="name" className="font-bold pr-2 ">
-                Expiry Date:
-              </label>
-              <input
-                type="text"
-                required
-                id="storeName"
-                onChange={handleChange}
-                value={storeName}
-                placeholder="MM/YY"
-                className="w-full rounded border-0 focus:border-[#1e6091] "
-              />
+            <div className="w-full   flex  md:flex-nowrap justify-between items-center mt-[10px]  px-4 ">
+              <div className="flex flex-col  items-start w-[49%] ">
+                <label htmlFor="name" className="font-bold pr-2 ">
+                  Expiry Date:
+                </label>
+                <input
+                  type="text"
+                  required
+                  id="storeName"
+                  onChange={handleChange}
+                  value={storeName}
+                  placeholder="MM/YY"
+                  className="w-full rounded border-0 focus:border-[#1e6091] "
+                />
+              </div>
+              <div className="flex flex-col  items-start  w-[49%]  ">
+                <label htmlFor="price" className="font-bold pr-2 ">
+                  CVV:
+                </label>
+                <input
+                  type="number"
+                  id="tel"
+                  required
+                  min="0"
+                  onChange={handleChange}
+                  value={tel}
+                  placeholder="Code"
+                  className="w-full rounded border-0 focus:border-[#1e6091] "
+                />
+              </div>
             </div>
-            <div className="flex flex-col  items-start  w-[49%]  ">
-              <label htmlFor="price" className="font-bold pr-2 ">
-                CVV:
-              </label>
-              <input
-                type="number"
-                id="tel"
-                required
-                min="0"
-                onChange={handleChange}
-                value={tel}
-                placeholder="Code"
-                className="w-full rounded border-0 focus:border-[#1e6091] "
-              />
-            </div>
-          </div>
 
-          <div className="flex items-center justify-center px-4 mt-[10px] mb-[70px]">
-            <button
-              type="submit"
-              className="w-full rounded border-0 h-[40px] outline-0 hover:bg-[#424b52] transition all duration-150 ease-in-out bg-[#1e6091] text-white uppercase flex items-center justify-center"
-            >
-              Pay Now
-            </button>
-          </div>
-        </form>
-      </main>
-    </>
+            <div className="flex items-center justify-center px-4 mt-[10px] mb-[70px]">
+              <button
+                type="submit"
+                className="w-full rounded border-0 h-[40px] outline-0 hover:bg-[#424b52] transition all duration-150 ease-in-out bg-[#1e6091] text-white uppercase flex items-center justify-center"
+              >
+                Pay Now
+              </button>
+            </div>
+          </form>
+        </main>
+        <Order />
+      </section>
+    </main>
   );
 }
 
