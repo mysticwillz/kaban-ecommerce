156b12234fd99dcd8fd88194a77bee4f09f517f8
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2023-01-09 13:37:48 +0100

    ref: this commit updates the footer component

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -7,82 +7,82 @@ function Footer() {
       <section className="max-w-7xl text-white  flex justify-between w-full flex-wrap">
         <ul>
           <li className=" text-[22px] font-bold">ABOUT KABAN</li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             contact us
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             about us
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             careers
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             our blog
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             forum
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             terms and condition{" "}
           </li>
         </ul>
         <ul>
           <li className=" text-[22px] font-bold">PAYMENT</li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             kaban pay
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             wallet
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             mastercard
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             visa
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             verve
           </li>
         </ul>
         <ul>
           <li className=" text-[22px] font-bold">BUYING ON KABAN</li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             buyers safety center
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             FAQs
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             delivery
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             kaban return policy
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             bulk purchase
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             digital services
           </li>
         </ul>
         <ul>
           <li className=" text-[22px] font-bold">MORE INFO</li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             privacy policy
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             site map
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             track my order
           </li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             authenticate item policy
           </li>
         </ul>
         <ul>
           <li className=" text-[22px] font-bold">MAKE MONEY ON KABAN</li>
-          <li className="capitalize text-[14px] hover:text-[17px] transition duration-250 ease-in-out cursor-pointer">
+          <li className="capitalize text-[14px] transition duration-250 ease-in-out cursor-pointer">
             become a kaban affiliate
           </li>
         </ul>
