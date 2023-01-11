5bb6d73ef2c88f702574ed2edefd0138435e6062
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2023-01-11 16:59:06 +0100

    feat: this commit  lists all the products on the home page

--- src/Molecules/splitterFunction.js
+++ src/Molecules/splitterFunction.js
@@ -1,6 +1,6 @@
 export const Split = (string) => {
-  if (string.length > 40) {
-    const newString = string.slice(0, 40);
+  if (string.length > 23) {
+    const newString = string.slice(0, 23);
     return `${newString}...`;
   }
   return string;
