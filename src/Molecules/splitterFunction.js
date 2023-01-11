93ab46d15c2bf38d7d2d8550ef650011677a2740
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2023-01-11 15:04:59 +0100

    this commit updates the molecules

--- src/Molecules/splitterFunction.js
+++ src/Molecules/splitterFunction.js
@@ -1,6 +1,6 @@
 export const Split = (string) => {
-  if (string.length > 50) {
-    const newString = string.slice(0, 20);
+  if (string.length > 40) {
+    const newString = string.slice(0, 40);
     return `${newString}...`;
   }
   return string;
